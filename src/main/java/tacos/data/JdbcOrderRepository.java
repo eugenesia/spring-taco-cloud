6664b0337411dc4f4ad8ad6a643ca7bafadb6b5c
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-02 14:29:51 +0100

    Fix missing include JdbcTemplate

--- src/main/java/tacos/data/JdbcOrderRepository.java
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -1,6 +1,7 @@
 package tacos.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
