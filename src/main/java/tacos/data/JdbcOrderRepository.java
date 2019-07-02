4a226e6c2ed8cd6e053390bf20c89c2fa8dc49a9
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-02 13:29:25 +0100

    Create JdbcOrderRepository

---
+++ src/main/java/tacos/data/JdbcOrderRepository.java
@@ -0,0 +1,34 @@
+package tacos.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import tacos.Order;
+
+public class JdbcOrderRepository implements OrderRepository {
+
+  private SimpleJdbcInsert orderInserter;
+  private SimpleJdbcInsert orderTacoInserter;
+  private ObjectMapper objectMapper;
+    
+  @Autowired
+  public JdbcOrderRepository(JdbcTemplate jdbc) {
+    this.orderInserter = new SimpleJdbcInsert(jdbc)
+        .withTableName("Taco_Order")
+        .usingGeneratedKeyColumns("id");
+    
+    this.orderTacoInserter = new SimpleJdbcInsert(jdbc)
+        .withTableName("Taco_Order_Tacos");
+    
+    this.objectMapper = new ObjectMapper();
+  }
+
+  @Override
+  public Order save(Order order) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
