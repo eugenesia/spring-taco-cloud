57318c95f4cc36981648cc8af980dc6b60dc56f0
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-03 17:01:34 +0100

    Add OrderRepository::findByDeliveryZip()

--- tacos-jpa/src/main/java/tacos/data/OrderRepository.java
+++ tacos-jpa/src/main/java/tacos/data/OrderRepository.java
@@ -1,9 +1,14 @@
 package tacos.data;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import tacos.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+  // Find all orders by delivery ZIP code. Spring Data automatically implements
+  // method based on its name.
+  List<Order> findByDeliveryZip(String deliveryZip);
 }
