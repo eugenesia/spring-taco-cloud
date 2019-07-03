6dd0d80cb52dd55c4e3447dc218dbc921718731d
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-03 16:19:05 +0100

    Annotate Order for JPA

--- tacos-jpa/src/main/java/tacos/Order.java
+++ tacos-jpa/src/main/java/tacos/Order.java
@@ -1,6 +1,15 @@
-//tag::all[]
-//tag::allButValidation[]
 package tacos;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,49 +19,50 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import lombok.Data;
 
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order") // Needed as table name different from class name.
+public class Order implements Serializable {
+  
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  
+  private Date placedAt;
 
-  //end::allButValidation[]
   @NotBlank(message="Name is required")
-  //tag::allButValidation[]
   private String name;
-  //end::allButValidation[]
 
   @NotBlank(message="Street is required")
-  //tag::allButValidation[]
   private String street;
-  //end::allButValidation[]
 
   @NotBlank(message="City is required")
-  //tag::allButValidation[]
   private String city;
-  //end::allButValidation[]
 
   @NotBlank(message="State is required")
-  //tag::allButValidation[]
   private String state;
-  //end::allButValidation[]
 
   @NotBlank(message="Zip code is required")
-  //tag::allButValidation[]
   private String zip;
-  //end::allButValidation[]
 
   @CreditCardNumber(message="Not a valid credit card number")
-  //tag::allButValidation[]
   private String ccNumber;
-  //end::allButValidation[]
 
   @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
            message="Must be formatted MM/YY")
-  //tag::allButValidation[]
   private String ccExpiration;
-  //end::allButValidation[]
 
   @Digits(integer=3, fraction=0, message="Invalid CVV")
-  //tag::allButValidation[]
   private String ccCVV;
-
+  
+  @ManyToMany(targetEntity=Taco.class)
+  private List<Taco> tacos = new ArrayList<>();
+  
+  public void addDesign(Taco design) {
+    this.tacos.add(design);
+  }
+  
+  @PrePersist
+  void placedAt() {
+    this.placedAt = new Date();
+  }
 }
-//end::allButValidation[]
-//end::all[]
